e47ee2624a9110e3c9f458a0a5dda000700651c2
cielavenir <cielartisan@gmail.com>
2013-12-16 15:36:39 +0900

    fixed Array#permutation2

--- libciel.rb
+++ libciel.rb
@@ -39,20 +39,19 @@ end
 
 class Array
 	def permutation2(n=self.size)
-		return Enumerator.new{} if n<0||self.size<n
-		Enumerator.new{|y|
-			a=self.sort
-			y<<a.dup[0,n]
-			while true
-				a=a[0,n]+a[n..-1].reverse
-				k=(a.size-2).downto(0).find{|i|a[i]<a[i+1]}
-				break if !k
-				l=(a.size-1).downto(k+1).find{|i|a[k]<a[i]}
-				a[k],a[l]=a[l],a[k]
-				a=a[0,k+1]+a[k+1..-1].reverse
-				y<<a.dup[0,n]
-			end
-		}
+		return to_enum(:permutation2) unless block_given?
+		return if n<0||self.size<n
+		a=self.sort
+		yield a.dup[0,n]
+		while true
+			a=a[0,n]+a[n..-1].reverse
+			k=(a.size-2).downto(0).find{|i|a[i]<a[i+1]}
+			break if !k
+			l=(a.size-1).downto(k+1).find{|i|a[k]<a[i]}
+			a[k],a[l]=a[l],a[k]
+			a=a[0,k+1]+a[k+1..-1].reverse
+			yield a.dup[0,n]
+		end
 	end
 end
 
